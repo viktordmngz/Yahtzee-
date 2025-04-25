affa14f1308c88ee6e87e405a5d05bf4159cecf2
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2025-04-24 23:23:34 -0400

    Update Yahtzee.py
    * Updated the introductory comments to explain the rules a bit more clearly

--- Yahtzee.py
+++ Yahtzee.py
@@ -12,11 +12,9 @@ LICENSE
 ========
 None, but some credit is always appreciated :)
 
-
 =====
 NOTE
 =====
-
 This code is intended to run in a terminal/console.
 If the code does not work with your computer, try commenting out the functions that require OS.
 Also, sometimes I add files to my GitHub that aren't complete just to have them there. I will mark this "COMPLETED" when finished.
@@ -24,11 +22,14 @@ Also, sometimes I add files to my GitHub that aren't complete just to have them 
 ======
 RULES
 ======
-
 Yahtzee is a two-player dice rolling game.
 
-Players take turns rolling dice (up to 5) and try to match their dice
-with the scoring categories.
+Players take turns rolling dice and try to match their dice
+with the scoring categories. Each player gets a maximum of 3 rolls.
+After each roll, the player selects how many dice they want to keep and roll the remaining dice.
+Once the player has rolled for the 3rd time, they must choose a category to apply their rolls to.
+The game ends once both players have filled in each of the 13 categories on their scorecards.
+
 
 Regular Scoring Categories:
 
@@ -41,22 +42,26 @@ Regular Scoring Categories:
 
 If a player reaches 63 points for these categories, they receive a bonus of 35 points.
 
+
 Special Scoring Categories:
 
-3 of a kind  --> 3 or more similar dice values;    score the whole hand
-4 of a kind  --> 4 or more similar dice values;    score the whole hand
-Full house   --> 2 of one number and 3 of another; 25 points
-Small Strait --> 4 consecutive numbers;            30 points
-Large Strait --> 5 consecutive numbers;            40 points
-Yahtzee      --> 5 of the same number;             50 points
-Chance       --> Can be used once a game;          score the whole hand
+3 of a kind  --> 3 or more similar dice values        score the whole hand
+4 of a kind  --> 4 or more similar dice values        score the whole hand
+Full house   --> 2 of one number and 3 of another     25 points
+Small Strait --> 4 consecutive numbers                30 points
+Large Strait --> 5 consecutive numbers                40 points
+Yahtzee      --> 5 of the same number                 50 points
+Chance       --> Can be used once a game              score the whole hand
 
-Special Scoring Rules:
+
+Yahtzee Bonus Rule:
+
 Once a Yahtzee is scored, a player can receive 100 bonus points for any other Yahtzees.
 Then, the points are scored according to the rules above.
 If the player has already marked the Yahtzee category with a 0, then no bonus is received.
 
-These rules come from the Hasbro rules, so feel free to change the scoring rules in the code below for any house rules you play by.
+
+These rules come from the Hasbro rules, so feel free to change or add to the scoring rules in the code below.
 '''
 
 from random import choice
